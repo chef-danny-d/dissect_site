830a011e3842b89efdc34b2aac3f8e435b1619cb
Dissect_Front_End <danielpapp@TechArks-MacBook-Air.local>
2016-09-28 21:57:16 -0400

    fixes made to the login and sign up page to improve encryption

--- login.php
+++ login.php
@@ -24,12 +24,13 @@
     #log button{
         width: 50%;
         margin-top: 5%;
+      }
 </style>
 <body  id="acc_body">
 <div id="wrapper log">
-    <img id="logimg" src="SynkMonster-Updated-Logo_V2_no-name.png" alt="...">
-<form action="profile.php" method="get">
-    <form action="profile.php" method="get">
+    <img id="logimg" src="img/SynkMonster-Updated-Logo_V2_no-name.png" alt="...">
+<form action="profile.php" method="get" enctype="application/x-www-form-urlencoded">
+    <form action="profile.php" method="get" enctype="application/x-www-form-urlencoded">
         <label style="text-align: center; width: 100%;">E-mail*</label> <input style="width: 50%; margin-left: 25%;" type="text" class="form-control" name="email" placeholder="example@example.com" required><br>
         <label style="text-align: center; width: 100%;">Password*</label> <input style="width: 50%; margin-left: 25%;" required type="password" class="form-control" name="password" placeholder="Password"><br>
         <button style="width: 50%; margin-left: 25%; margin-top: 1.5em;" type="submit" class="btn btn-default">Login</button>
--- signup.php
+++ signup.php
@@ -2,6 +2,9 @@
 <?php include "script.php"; ?>
 <body  id="acc_body">
 <style>
+    from{
+      padding-bottom: 5em;
+    }
     #sign form{
         width: 50%;
         margin-left: auto;
@@ -28,8 +31,8 @@
     }
 </style>
 <div id="wrapper sign">
-    <img id="signimg" src="SynkMonster-Updated-Logo_V2_no-name.png" alt="...">
-    <form action="profile.php" method="get">
+    <img id="signimg" src="img/SynkMonster-Updated-Logo_V2_no-name.png" alt="...">
+    <form action="profile.php" method="get" enctype="application/x-www-form-urlencoded" >
         <label style="text-align: center; width: 100%;">Name<em>*required</em></label> <input style="width: 50%; margin-left: 25%;" type="text" class="form-control" name="name" placeholder="Full Name" required><br>
         <label style="text-align: center; width: 100%;">E-mail<em>*required</em></label> <input style="width: 50%; margin-left: 25%;" type="text" class="form-control" name="email" placeholder="example@example.com" required><br>
         <label style="text-align: center; width: 100%;">Password<em>*required</em></label> <input style="width: 50%; margin-left: 25%;" required type="password" class="form-control" name="password" placeholder="Password"></br>
